7d1afc71d5729cc5543b51774c6db14142e4f7dd
Don Nguyen <don.nguyen@hazuu.com>
2017-08-10 10:48:55 +0700

    Add wdActiveMe directive to active floating label if needed

--- dist/form-builder.js
+++ dist/form-builder.js
@@ -1518,3 +1518,24 @@ angular.module('mwFormBuilder')
             }
         }
     }]);
+
+angular.module('mwFormBuilder')
+    .directive('wdActiveMe', ["$timeout", "$parse", function($timeout, $parse) {
+        return {
+            link: function(scope, element, attrs) {
+                $timeout(function() {
+                    elementID = element[0].id;
+                    parentElement = element[0].parentElement;
+                    labelElement = parentElement.querySelector('label[for="' + elementID + '"]') || parentElement.querySelector('label');
+                    jElement = angular.element(element);
+
+                    if (jElement.val().length > 0 || jElement.attr('placeholder') !== undefined || jElement[0].validity.badInput === true) {
+                        angular.element(labelElement).addClass('active')
+                    } else {
+                        angular.element(labelElement).removeClass('active')
+                    }
+                });
+            }
+        };
+    }])
+
